0aea90a5757e6be6ac1b5d7b348d6e6b3594d985
Nhan Nguyen <48123439+edwardnguyen225@users.noreply.github.com>
2024-04-28 14:35:07 +0700

    feat(app): Add needed tags; Finish footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import TheFooter from './components/TheFooter.vue'
 </script>
 
 <template>
@@ -14,84 +15,115 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-    Product
 
-    Overview
-    Pricing
-    Marketplace
-    Features
-    Integrations
+    <nav>
+      <ul>
+        <li>
+          Product
+          <ul>
+            <li>Overview</li>
+            <li>Pricing</li>
+            <li>Marketplace</li>
+            <li>Features</li>
+            <li>Integrations</li>
+          </ul>
+        </li>
+        <li>
+          Company
+          <ul>
+            <li>About</li>
+            <li>Team</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </li>
+        <li>
+          Connect
+          <ul>
+            <li>Contact</li>
+            <li>Newsletter</li>
+            <li>LinkedIn</li>
+          </ul>
+        </li>
+      </ul>
+      <button>Login</button>
+      <button>Login</button>
+    </nav>
 
-    Company
+    <div>
+      <h2>A modern publishing platform</h2>
+      <p>Grow your audience and build your online brand</p>
+      <div>
+        <button>Start for Free</button>
+        <button>Learn More</button>
+      </div>
+    </div>
 
-    About
-    Team
-    Blog
-    Careers
+    <section>
+      <h2>Designed for the future</h2>
 
-    Connect
+      <div>
+        <img
+          alt="Editor"
+          src="./assets/images/illustration-editor-mobile.svg"
+          width="326"
+          height="326"
+        />
+      </div>
 
-    Contact
-    Newsletter
-    LinkedIn
+      <div>
+        <h3>Introducing an extensible editor</h3>
+        <p>
+          Blogr features an exceedingly intuitive interface which lets you focus on one thing:
+          creating content. The editor supports management of multiple blogs and allows easy
+          manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins
+          and themes provide easy ways to add functionality or change the looks of a blog.
+        </p>
+      </div>
 
-    Login
-    Sign Up
+      <div>
+        <h3>Robust content management</h3>
+        <p>
+          Flexible content management enables users to easily move through posts. Increase the
+          usability of your blog by adding customized categories, sections, format, or flow. With
+          this functionality, you're in full control.
+        </p>
+      </div>
+    </section>
 
-    A modern publishing platform
-    Grow your audience and build your online brand
+    <section>
+      <img alt="Editor" src="./assets/images/illustration-phones.svg" width="328" height="347" />
+      <h2>State of the Art Infrastructure</h2>
+      <p>
+        With reliability and speed in mind, worldwide data centers provide the backbone for
+        ultra-fast connectivity. This ensures your site will load instantly, no matter where your
+        readers are, keeping your site competitive.
+      </p>
+    </section>
 
-    Start for Free
-    Learn More
-
-    Designed for the future
-
-    Introducing an extensible editor
-    Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
-    The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
-    videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
-    change the looks of a blog.
-
-    Robust content management
-    Flexible content management enables users to easily move through posts. Increase the usability of your blog
-    by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
-
-    State of the Art Infrastructure
-    With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity.
-    This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
-
-    Free, open, simple
-    Blogr is a free and open source application backed by a large community of helpful developers. It supports
-    features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools,
-    and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
-
-    Powerful tooling
-    Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze,
-    but
-    capable of producing even the most complicated sites.
-
-    Product
-
-    Overview
-    Pricing
-    Marketplace
-    Features
-    Integrations
-
-    Company
-
-    About
-    Team
-    Blog
-    Careers
-
-    Connect
-
-    Contact
-    Newsletter
-    LinkedIn
-
+    <section>
+      <img alt="Editor" src="./assets/images/illustration-phones.svg" width="328" height="347" />
+      <div>
+        <h2>Free, open, simple</h2>
+        <p>
+          Blogr is a free and open source application backed by a large community of helpful
+          developers. It supports features such as code syntax highlighting, RSS feeds, social media
+          integration, third-party commenting tools, and works seamlessly with Google Analytics. The
+          architecture is clean and is relatively easy to learn.
+        </p>
+      </div>
+      <div>
+        <h2>Powerful tooling</h2>
+        <p>
+          Batteries included. We built a simple and straightforward CLI tool that makes
+          customization and deployment a breeze, but capable of producing even the most complicated
+          sites.
+        </p>
+      </div>
+    </section>
   </main>
+
+  <TheFooter />
 </template>
 
 <style scoped>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,130 @@
+<template>
+  <footer class="footer-container">
+    <div class="content-container">
+      <img alt="Blogr logo" class="logo" src="../assets/images/logo.svg" width="101" height="40" />
+
+      <div class="content">
+        <div class="content-section">
+          <h4>Product</h4>
+          <ul>
+            <li><a href="#" class="link">Overview</a></li>
+            <li><a href="#" class="link">Pricing</a></li>
+            <li><a href="#" class="link">Marketplace</a></li>
+            <li><a href="#" class="link">Features</a></li>
+            <li><a href="#" class="link">Integrations</a></li>
+          </ul>
+        </div>
+
+        <div class="content-section">
+          <h4>Company</h4>
+          <ul>
+            <li><a href="#" class="link">About</a></li>
+            <li><a href="#" class="link">Team</a></li>
+            <li><a href="#" class="link">Blog</a></li>
+            <li><a href="#" class="link">Careers</a></li>
+          </ul>
+        </div>
+
+        <div class="content-section">
+          <h4>Connect</h4>
+          <ul>
+            <li><a href="#" class="link">Contact</a></li>
+            <li><a href="#" class="link">Newsletter</a></li>
+            <li><a href="#" class="link">LinkedIn</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer-container {
+  padding: 75px 24px;
+  background-color: #24242c;
+  border-top-right-radius: 100px;
+
+  font-family: 'Ubuntu';
+  font-size: 18px;
+  color: white;
+}
+
+.footer-container .content-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.footer-container .content-container .logo {
+  margin-bottom: 72px;
+}
+
+.footer-container .content-container .content {
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+}
+
+.footer-container .content-container .content .content-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 21px;
+}
+
+.footer-container .content-container .content .content-section h4 {
+  font-weight: 500;
+}
+
+ul {
+  padding: 0;
+  list-style-type: none;
+}
+
+li {
+  text-align: center;
+}
+
+li + li {
+  margin-top: 12px;
+}
+
+a.link {
+  text-decoration: none;
+  color: white;
+  opacity: 0.75;
+}
+
+a.link:hover {
+  text-decoration: underline;
+}
+
+@media (min-width: 768px) {
+  .footer-container .content-container {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 1110px;
+    flex-direction: row;
+    align-items: start;
+    justify-content: space-between;
+    gap: calc(184 / 1440 * 100%);
+  }
+
+  .footer-container .content-container .content {
+    flex: 1 1 auto;
+    flex-direction: row;
+    align-items: start;
+    gap: 30px;
+  }
+
+  .footer-container .content-container .content .content-section {
+    width: calc(255 / 1440 * 100%);
+    flex: 1 1 auto;
+    align-items: start;
+  }
+
+  .footer-container .content-container .content .content-section li {
+    text-align: left;
+  }
+}
+</style>
